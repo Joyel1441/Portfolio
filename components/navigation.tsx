2bc429e25138c52477be8e7473f9034a50d88bd9
Joyel1441 <55022719+Joyel1441@users.noreply.github.com>
2023-02-04 13:53:00 +0530

    updated navbar size on mobile screens

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -5,7 +5,7 @@ const Navigation = () => {
   return (
     <nav className="flex justify-center h-10 mt-2">
       <div className="flex bg-blue-200 rounded-xl p-4 items-center justify-center">
-        <ul className="flex flex-row space-x-20 xl:space-x-40 relative">
+        <ul className="flex flex-row space-x-10 xl:space-x-40 relative">
           <li className="hover:bg-blue-300 rounded-xl p-1 z-10"><Link href={"/"}>Home</Link></li>
           <li className="hover:bg-blue-300 rounded-xl p-1"><Link href={"/projects"}>Projects</Link></li>
           <li className="hover:bg-blue-300 rounded-xl p-1"><Link href={"/contact"}>Contact</Link></li>
