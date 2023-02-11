4ace08b2d420ce7b630689062e272a8bdc100977
Joyel1441 <55022719+Joyel1441@users.noreply.github.com>
2023-02-11 23:17:09 +0530

    update

--- app/contact/loading.tsx
+++ app/contact/loading.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div>
+      <p>Loading...</p>
+    </div>
+  );
 };
 
 export default Loading;
