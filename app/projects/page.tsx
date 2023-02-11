a373f017218bc179510948e60df79fd5be0e85c1
Joyel1441 <55022719+Joyel1441@users.noreply.github.com>
2023-02-11 22:41:22 +0530

    updated

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,4 @@
 import ProjectCard from "@/components/projectcard";
-import Image from "next/image";
 import {prisma} from '../../prisma/index';
 
 async function getProjects() {
@@ -29,7 +28,7 @@ const Projects = async () => {
     <div className="flex w-screen h-full justify-center items-center mt-4">
       <div className="grid grid-cols-1 md:grid-cols-2 mt-10 mb-20 md:mt-20 gap-4">
     {data.map((project) => (
-        <div key={project.id} className="p-4 w-full border-2 border-solid border-blue-200 rounded-xl hover:border-blue-700 hover:-translate-y-2 hover:shadow-md hover:shadow-blue-500 max-w-sm md:max-w-md">
+        <div key={project.id} className="p-4 w-full border-2 border-solid border-blue-200 rounded-xl hover:border-blue-700 hover:-translate-y-2 hover:shadow-md hover:shadow-blue-500 max-w-sm md:max-w-md mt-10">
           <ProjectCard id={project.id} project_title={project.project_title} project_description={project.project_description} github_link={project.github_link} demo_link={project.demo_link} image_link={project.image_link}/>
         </div>
       ))}
