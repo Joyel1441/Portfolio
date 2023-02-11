b1fd0d05b1d4911324ae0308c7821cf6bc97dda1
Joyel1441 <55022719+Joyel1441@users.noreply.github.com>
2023-02-11 23:01:38 +0530

    update

--- components/projectcard.tsx
+++ components/projectcard.tsx
@@ -12,19 +12,18 @@ type Props = {
 };
 
 const ProjectCard = (props: Props) => {
-  const { id } = props;
   const { project_title } = props;
   const { project_description } = props;
   const { github_link } = props;
   const { demo_link } = props;
   const { image_link } = props;
   return (
-    <div className="text-xl">
+    <div className="text-sm md:text-xl">
       <div className="grid grid-flow-row gap-2">
         <div className="w-full block relative h-72">
         <Image src={image_link} alt="project image" fill={true} className="rounded-md"/>
         </div>
-        <h1 className="font-bold text-2xl">{project_title}</h1>
+        <h1 className="font-bold text-lg md:text-2xl">{project_title}</h1>
         <p>{project_description}</p>
         {github_link.length > 0 && <span className="w-max"><Link href={github_link} className="text-blue-700">Source code</Link></span>}
         {demo_link.length > 0 && <span className="w-max"><Link href={demo_link} className="text-blue-700">Visit</Link></span>}
